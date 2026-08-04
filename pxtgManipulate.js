b132c3497f2c53a2f65db257e38b5669b1115217
Marcelo Giazzon <marcelo.giazzon@gmail.com>
2026-08-04 06:22:11 -0300

    .

---
+++ pxtgManipulate.js
@@ -0,0 +1,9 @@
+function pxtgManipulate(pText)
+{
+	var result = "I manipulate this string by external js... " + pText;
+
+	console.log("pxtgManipulate (external GitHub js) || input: " + pText);
+	console.log("pxtgManipulate (external GitHub js) || output: " + result);
+
+	return result;
+}
